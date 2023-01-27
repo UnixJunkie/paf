8245302c22191032dad6d337f7f7320871e16c58
Francois Berenger <unixjunkie@sdf.org>
2023-01-27 12:41:58 +0900

    compiles

--- src/paf.ml
+++ src/paf.ml
@@ -9,25 +9,54 @@ module A = BatArray
 *)
 
 let to_chan chan x =
-  Marshal.(to_channel chan x [No_sharing, Closures])
-
+  Marshal.(to_channel chan x [No_sharing; Closures])
+    
 let from_chan chan =
   Marshal.from_channel chan
 
 (* send integers *)
-let demux max_count csize out_pipes () =
-  let output_channels = A.map Unix.out_channel_of_descr out_pipes in
+let demux max_count _csize out_chans =
   let pipe_index = ref 0 in
-  let n_pipes = A.length output_channels in
+  let n_pipes = A.length out_chans in
   for i = 0 to max_count - 1 do
-    let pipe = output_channels.(!pipe_index) in
+    let pipe = out_chans.(!pipe_index) in
     to_chan pipe i;
     pipe_index := (!pipe_index + 1) mod n_pipes
   done;
-  A.iter Unix.close out_pipes
+  A.iter close_out out_chans
+
+(* increment received int; send it out *)
+let work in_pipe out_pipe =
+  try to_chan out_pipe (1 + (from_chan in_pipe))
+  with End_of_file -> begin
+      close_in in_pipe;
+      close_out out_pipe
+    end
+
+let mux in_pipes =
+  let total = ref 0 in
+  let num_pipes = A.length in_pipes in
+  let n = ref num_pipes in
+  let opened = A.make num_pipes true in
+  let i = ref 0 in
+  while !n > 0 do
+    (if opened.(!i) then
+       let in_pipe = in_pipes.(!i) in       
+       try
+         let x = from_chan in_pipe in
+         total := !total + x
+       with End_of_file ->
+         begin
+           close_in in_pipe;
+           opened.(!i) <- false
+         end
+    );
+    i := (!i + 1) mod num_pipes
+  done;
+  Printf.printf "total: %d\n" !total
 
 let main () =
-  let nprocs = int_of_string Sys.argv.(1) in
+  let _nprocs = int_of_string Sys.argv.(1) in
   failwith "not implemented yet"
 
 let () = main ()
